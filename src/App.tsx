a2f4601e09bd42b7e65d598dec4689528608d66e
Aditya Gantayat <adityanarayan10@gmail.com>
2025-04-30 20:29:45 +0530

    removal of test code

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Home from './components/Home';
-import { MyComponent } from './components/MyComponent';
+// import { MyComponent } from './components/MyComponent';
 
 function App() {
   return (
